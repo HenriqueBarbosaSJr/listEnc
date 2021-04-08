fe817e4de997bf9c841ae1a2052c57e2e58223e8
Henrique Barbosa de Souza Jr <henrique@hbveterinaria.com.br>
2021-04-08 19:54:33 -0300

    Leitura dos dados da lista

--- listEnc.c
+++ listEnc.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct aluno
 {
-    char nome[15];
+    char nome[20];
     int idade;
     struct aluno * proximo;
        
@@ -18,5 +18,22 @@ int main(){
     ini_aluno = (Aluno *)malloc(sizeof(Aluno));
     proximo_aluno = ini_aluno;
 
+    while (1)
+    {
+        printf("\n");
+        
+        printf("digite o nome = ");
+        fgets(proximo_aluno->nome,20,stdin);
+        
+        printf("digite a idade = ");
+        scanf("%i", &proximo_aluno->idade);
+
+        
+
+        //printf("%s",proximo_aluno->nome);
+
+
+    }
     
+
 }
